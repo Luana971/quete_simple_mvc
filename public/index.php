cbf9251c44ec3e7195f8fd40eaec7a2c36815139
Lüana Lorthios <luanalorthios@windowslive.com>
2018-10-01 16:28:52 +0200

    Ajout modèle MVC

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
-    require __DIR__ . '/../src/Controller/ItemController.php';
+
+    require __DIR__ . '/../vendor/autoload.php';
+    use Controller\ItemController;
+    use Model\ItemManager;
+
+    $items = new ItemController();
+    $items->index();
+
 ?>
