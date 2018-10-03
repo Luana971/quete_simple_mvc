1ddb9f89365763daad42cbfcb924b9341c04a634
Lüana Lorthios <luanalorthios@windowslive.com>
2018-10-03 14:22:44 +0200

    ajout category

--- src/View/categories.php
+++ src/View/categories.php
@@ -10,10 +10,10 @@
 </head>
 <body>
     <section>
-        <h1>Items</h1>
+        <h1>Categories</h1>
             <ul>
-            <?php foreach ($items as $item) : ?>
-                <li><?= $item['title'] ?></li>
+            <?php foreach ($categories as $category) : ?>
+                <li><?= $category['name'] ?></li>
             <?php endforeach ?>
             </ul>
         </section>
